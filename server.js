2a859a9a3afcffa15b30b14b753d534d04427d83
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-04-27 21:49:15 +0700

    Add authors path

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
@@ -20,5 +21,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 4000)
